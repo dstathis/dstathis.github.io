498d36ce59b481bbd65ba275cefa7204cfbfb956
Dylan Stephano-Shachter <dylan@theone.ninja>
2024-06-12 20:41:17 +0300

    basic rendering

---
+++ render.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Copyright 2024 Dylan Stephano-Shachter
+# See CODE_LICENSE file for licensing details.
+
+import pathlib
+import tomllib
+
+import jinja2
+
+def main():
+    posts = []
+    posts_dir = pathlib.Path('posts')
+    for post_file in posts_dir.iterdir():
+        with post_file.open('rb') as f:
+            post = tomllib.load(f)
+        posts.append(post)
+
+    web_dir = pathlib.Path('www')
+    web_dir.mkdir(exist_ok=True)
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'), autoescape=True)
+    index = env.get_template('index.html')
+    with (web_dir / 'index.html').open('w') as f:
+        f.write(index.render(posts=posts))
+
+
+if __name__ == '__main__':
+    main()
