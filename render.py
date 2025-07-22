a59bb49c5390b52daf31a1418b7180ac4eb9f553
Dylan Stephano-Shachter <dylan@theone.ninja>
2025-07-22 18:48:08 +0300

    Greece Coffee

--- render.py
+++ render.py
@@ -27,6 +27,14 @@ def main():
         ath_posts.append(post)
     ath_posts.sort(key=lambda x: x['name'])
 
+    greece_coffee_posts = []
+    greece_coffee_posts_dir = pathlib.Path('posts/greece_coffee')
+    for post_file in greece_coffee_posts_dir.iterdir():
+        with post_file.open('rb') as f:
+            post = tomllib.load(f)
+        greece_coffee_posts.append(post)
+    greece_coffee_posts.sort(key=lambda x: x['name'])
+
     env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'), autoescape=False)
     index = env.get_template('index.html')
     with pathlib.Path('index.html').open('w') as f:
@@ -34,6 +42,9 @@ def main():
     athens = env.get_template('athens.html')
     with pathlib.Path('athens.html').open('w') as f:
         f. write(athens.render(posts=ath_posts))
+    greece_coffee = env.get_template('greece_coffee.html')
+    with pathlib.Path('greece_coffee.html').open('w') as f:
+        f. write(greece_coffee.render(posts=greece_coffee_posts))
 
 
 if __name__ == '__main__':
